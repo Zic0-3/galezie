6f3781ae7c86378634bb79cc2d449eecd93a2f0f
Zic0 <kornelburdzy@gmail.com>
2020-12-04 12:54:14 +0100

    tak bylo nie zmyslam

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
 public class Main {
+    static void zmien(int[] tab, int a, int b){
+        int x = tab[a];
+        tab[a] = tab[b];
+        tab[b] = x;
+    }
+
+    static void piszElemnty(int[] tab){
+        for(int i=0; i<8; i++){
+            System.out.println(tab[i]);
+        }
+    }
 
     public static void main(String[] args) {
-	System.out.println("Taj było biue zmyslam");
+        int[] tablica = {2,4,5,7,7,2,4,1,9};
+        zmien(tablica, 0, 2);
+        piszElemnty(tablica);
     }
 }
